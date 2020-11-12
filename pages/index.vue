ced574d8c3aa1d5c1b77aeea09afd56109ccf50a
Yutaka Omido <oomidou@lyxis.com>
2020-11-12 13:44:01 +0900

    キャッシュを利用しないよう変更

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,12 @@
     <b-card class="my-2"
             v-for="todo in highlightedTodoList" 
             :key="todo.objectID">
-      <b-card-tltle v-html="todo.title"></b-card-tltle>
+      <b-card-title v-html="todo.title"></b-card-title>
       <p v-html="todo.description"></p>
-      <b-button v-if="!todo.done" variant="primary" @click="makeTodoDone(todo)">完了</b-button>
+      <b-button v-if="!todo.done" variant="primary" @click="makeTodoDone(todo.objectID)">完了</b-button>
       <b-button v-else disabled>完了済</b-button>
-      <b-button @click="openUpdateModal(todo)">編集</b-button>
-      <b-button @click="deleteTodo(todo)">削除</b-button>
+      <b-button @click="openUpdateModal(todo.objectID)">編集</b-button>
+      <b-button @click="deleteTodo(todo.objectID)">削除</b-button>
     </b-card>
 
     <!-- 登録用ダイアログ -->
@@ -50,9 +50,12 @@
 
 <script>
 import * as algoliasearch from 'algoliasearch'
+import { createNullCache } from '@algolia/cache-common'
 import config from '~/algolia.config.js'
 
-const client = algoliasearch(config.appId, config.apiKey)
+const client = algoliasearch(config.appId, config.apiKey, {
+  responsesCache: createNullCache()
+})
 const index = client.initIndex('todo')
 
 index.setSettings({
@@ -82,7 +85,7 @@ export default {
       },
       registerModalIsVisible: false,
       updateModalIsVisible: false,
-      updateTargetTodo: null
+      updateTargetObjectID: null
     }
   },
   methods: {
@@ -97,37 +100,42 @@ export default {
     async registerTodo () {
       let todo = Object.assign({}, this.todoInput)
       let content = await index.saveObject(todo, { autoGenerateObjectIDIfNotExist: true })
-      todo.objectID = content.objectID
-      this.todoList.push(todo)
+      await this.searchTodo()
       this.clearInput()
     },
-    openUpdateModal (todo) {
+    openUpdateModal (objectID) {
+      const todo = this.getTodoByObjectID(objectID)
+      console.log(todo)
       this.todoInput.title = todo.title
       this.todoInput.description = todo.description
-      this.updateTargetTodo = todo
+      this.updateTargetObjectID = objectID
       this.updateModalIsVisible = true
     },
     async updateTodo () {      
-      this.updateTargetTodo.title = this.todoInput.title
-      this.updateTargetTodo.description = this.todoInput.description
-      await index.saveObject(this.updateTargetTodo)
+      await index.saveObject({
+        objectID: this.updateTargetObjectID,
+        ...this.todoInput
+      })
+      await this.searchTodo()
       this.clearInput()
-      this.updateTargetTodo = null
     },
-    async deleteTodo (todo) {
-      await index.deleteObject(todo.objectID)
-      this.todoList = this.todoList.filter(e => e.objectID !== todo.objectID)
+    async deleteTodo (objectID) {
+      await index.deleteObject(objectID)
+      await this.searchTodo()
     },
-    async makeTodoDone (todo) {
-      todo.done = true
+    async makeTodoDone (objectID) {
       await index.partialUpdateObject({
-        objectID: todo.objectID,
+        objectID: objectID,
         done: true
       })
+      await this.searchTodo()
     },
     async searchTodo () {
       let searchResult = await index.search(this.query)
       this.todoList = searchResult.hits
+    },
+    getTodoByObjectID(objectID) {
+      return this.todoList.filter(todo => todo.objectID === objectID)[0]
     }
   },
   computed: {
